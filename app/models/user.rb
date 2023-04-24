ccda2117cf4d02d8d69ab0623bda2e7129409747
Victor Malukhin <victor.malukhin@gmail.com>
2023-04-24 22:56:54 +0300

    Signup and login page templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, and :omniauthable
+  # :recoverable, :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 end
